fa67c0efa9bf2b0f7e1a35c23d9fa1398d1d7547
Kit Menke <cwmenke@gmail.com>
2025-05-26 20:03:37 -0500

    Working inner join

--- sqlviz/main.py
+++ sqlviz/main.py
@@ -16,11 +16,11 @@ class InnerJoin(Scene):
     def construct(self):
         # Visual representation of the inner join operation
         # Show the SQL query at the top of the screen
-        query = "SELECT person_id, favorite_color, person_name\nFROM color INNER JOIN person ON colors.person_id = person.id"
+        query = "SELECT person_id, favorite_color, person_name "
         sql_query = Text(query, font_size=32)
-
-        sql_query.to_edge(UP)
-        self.play(Write(sql_query))
+        sql_query_from = Text("FROM color INNER JOIN person ON colors.person_id = person.id", font_size=32)
+        query_group = VGroup(sql_query, sql_query_from).arrange(DOWN, aligned_edge=LEFT).to_edge(UP)
+        self.play(Write(query_group))
 
         # Add "color" label above the colors table
         colors_data = [["1", "Blue"], ["3", "Red"], ["9", "Green"]]
@@ -33,31 +33,13 @@ class InnerJoin(Scene):
         person_table = Table(person_data, col_labels=[Text("id", weight=BOLD), Text("person_name", weight=BOLD)], include_outer_lines=True).scale(0.4).shift(3*RIGHT+0.5*UP)
         person_label = Text("person").next_to(person_table, UP)
         
-        # person_table.add(SurroundingRectangle(person_table.get_columns()[0], color=YELLOW))
         self.play(Write(person_label), Create(person_table))
         self.wait(1)
 
-
-        joined_data = [["person_id", "favorite_color", "person_name",]]
-        row_num = 0
-        for colors_row in colors_data:
-            person_id = colors_row[0]
-            found = scan(person_data, person_id)
-            if found is not None:
-                person_row = person_data[found]
-                colors_table.add_highlighted_cell((row_num+2, 1), color=GREEN)
-                self.wait(0.2)
-                self.play(Indicate(person_table.get_cell((found+2, 1))))  # animate the highlighting
-                person_table.add_highlighted_cell((found+2, 1), color=GREEN)
-                self.wait(0.2)
-                new_row = [person_row[0], colors_row[1], person_row[1]]
-                joined_data.append(new_row)
-            else:
-                colors_table.add_highlighted_cell((row_num+2, 1), color=RED)
-                self.wait(0.2)
-            
-            self.wait(1)
-            row_num += 1
+        ##########################################################################################
+        # Animate the inner join
+        ##########################################################################################
+        joined_data = self.perform_inner_join(colors_table, colors_data, person_table, person_data)
 
         joined_table = Table(joined_data, include_outer_lines=True).scale(0.4).to_edge(DOWN)
         joined_label = Text("Result").next_to(joined_table, UP)
@@ -107,4 +89,51 @@ class InnerJoin(Scene):
         person_table.remove(rect1)
         joined_table.remove(rect2)
         self.play(Uncreate(rect1), Uncreate(rect2))
+    
+    def perform_inner_join(self, left_table, left_data, right_table, right_data):
+        # todo: refactor to allow specifying the columns
+        joined_data = [["person_id", "favorite_color", "person_name"]]
+        left_row_num = 0
+        right_row_num = 0
+        wait_time = 0.5
+        while left_row_num < len(left_data) or right_row_num < len(right_data):
+            if left_row_num >= len(left_data):
+                # If we have exhausted the left table, we can only take from the right table
+                right_table.add_highlighted_cell((right_row_num+2, 1), color=RED)
+                self.wait(wait_time)
+                right_row_num += 1
+                continue
+
+            if right_row_num >= len(right_data):
+                # If we have exhausted the right table, we can only take from the left table
+                left_table.add_highlighted_cell((left_row_num+2, 1), color=RED)
+                self.wait(wait_time)
+                left_row_num += 1
+                continue
+                
+            left_row = left_data[left_row_num]
+            right_row = right_data[right_row_num]
+
+            # TODO: make this index configurable
+            if left_row[0] == right_row[0]:
+                # successful inner join!
+                self.play(Indicate(left_table.get_cell((left_row_num+2, 1))))
+                left_table.add_highlighted_cell((left_row_num+2, 1), color=GREEN)
+                self.play(Indicate(right_table.get_cell((right_row_num+2, 1))))
+                right_table.add_highlighted_cell((right_row_num+2, 1), color=GREEN)
+                self.wait(wait_time)
+                # TODO: make this configurable
+                new_row = [right_row[0], left_row[1], right_row[1]]
+                joined_data.append(new_row)
+                left_row_num += 1
+                right_row_num += 1
+            elif left_row[0] < right_row[0]:
+                left_table.add_highlighted_cell((left_row_num+2, 1), color=RED)
+                self.wait(wait_time)
+                left_row_num += 1
+            elif left_row[0] > right_row[0]:
+                right_table.add_highlighted_cell((right_row_num+2, 1), color=RED)
+                self.wait(wait_time)
+                right_row_num += 1
         
+        return joined_data
